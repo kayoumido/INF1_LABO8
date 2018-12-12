8748a3f868cddc52f7ccfa2195384a9f1f71574e
Doran Kayoumi <dorankayoumi@gmail.com>
2018-12-12 10:48:32 +0100

    Add file header

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
+/*
+-----------------------------------------------------------------------------------
+Laboratoire : Labo08
+Fichier     : Labo08.cpp
+Auteur(s)   : Ilias Goujgali, Benoit Perret, Doran Kayoumi
+Date        : 12.12.2018
+
+But         :
+
+Remarque(s) :
+
+Compilateur : MinGW-g++ <6.3.0>
+-----------------------------------------------------------------------------------
+ */
+
 #include <iostream>
 
 int main() {
