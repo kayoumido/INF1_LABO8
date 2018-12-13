e7b053225e0f4086e08aa56eed6a62a5b704a4f2
Doran Kayoumi <dorankayoumi@gmail.com>
2018-12-13 10:31:56 +0100

    Rename typedef header file

---
+++ global.h
@@ -0,0 +1,4 @@
+#ifndef LABO08_GLOBAL_H
+#define LABO08_GLOBAL_H
+typedef char board[7][7];
+#endif
--- typedef.h
+++
@@ -1,4 +0,0 @@
-#ifndef LABO08_TYPEDEF_H
-#define LABO08_TYPEDEF_H
-typedef char board[7][7];
-#endif
