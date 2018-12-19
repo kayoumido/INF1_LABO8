6b5aaa9e1b29949efca0316c4f22866c6b33814a
Benoit P <10925370+nwabeen@users.noreply.github.com>
2018-12-19 11:56:16 +0100

    Vérification des entrées

--- main.cpp
+++ main.cpp
@@ -14,21 +14,43 @@ Compilateur : MinGW-g++ <6.3.0>
  */
 
 #include <iostream>
+#include <sstream>
+
 #include "display.h"
 #include "rules.h"
+bool endOfGame = false;
+const std::string ERROR_MSG = "Error message";
+const char HELP = 'h';
+const char WITHDRAW = 'q';
 
 using namespace std;
 
 int main() {
-  board board;
-  generate(board);
-  display(board);
-
-  string move = "42r";
-  if (isLegalMove(board, move)) {
-    movePeg(board, move);
+    board board;
+    generate(board);
     display(board);
-  }
 
-  return 0;
+    string move;
+    while (!endOfGame) {
+        if (cin >> move) {
+            if (isLegalMove(board, move) && move.length() == 3 && !isdigit(move[2])) {
+                movePeg(board, move);
+                display(board);
+
+            } else {
+                switch (move[0]) {
+                    case HELP:
+                        // Fonction help à rajouter
+                        break;
+                    case WITHDRAW:
+                        endOfGame == true;
+                        break;
+                    default:
+                        cout << ERROR_MSG << endl;
+                        display(board);
+                }
+            }
+        }
+    }
+    return 0;
 }
