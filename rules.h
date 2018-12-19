a2ae1ead509d04aaccd832a8e104abc78d388277
Doran Kayoumi <dorankayoumi@gmail.com>
2018-12-19 10:46:47 +0100

    Move coordinates typedef into global header

--- rules.h
+++ rules.h
@@ -5,13 +5,8 @@
 const unsigned short INPUT_ROW_POSITION = 0;
 const unsigned short INPUT_COL_POSITION = 1;
 const unsigned short INPUT_DIRECTION_POSITION = 2;
-const unsigned short COORDINATES_SIZE = 2;
 const unsigned short ASCII_NUMBER_START = 48;
 const unsigned MOVEMENT_SIZE = 2;
-const unsigned short I_COORDINATE = 0;
-const unsigned short J_COORDINATE = 1;
-
-typedef unsigned coordinates[COORDINATES_SIZE];
 
 bool isLegalMove(board board, std::string move);
 void getCoordinates(coordinates currentCoordinates, coordinates newCoordinates, char direction, unsigned movement_size);
