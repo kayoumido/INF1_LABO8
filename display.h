ed9054bd715347cd404537f6edd72e81c3a2c473
Benoit P <10925370+nwabeen@users.noreply.github.com>
2018-12-20 11:20:00 +0100

    Completed doxygen comments

--- display.h
+++ display.h
@@ -2,76 +2,120 @@
 * @file display.h
 * @brief This file contains the functions that are used to dipslay the board and some messages
 */
-#include "global.h"
 #include <vector>
+#include "global.h"
 
+/** 
+ * Help message
+*/
 const std::string HELP_MESSAGE = "Deplacements possibles:";
+
+/** 
+ * Error message
+*/
 const std::string ERROR_MESSAGE = "Deplacement invalide!";
+
+/** 
+ * Quit game message
+*/
 const std::string QUIT_GAME_MESSAGE = "Vous avez quittez la partie";
+
+/** 
+ * Perfect game message
+*/
 const std::string PERFECT_MESSAGE = "Parfait, il ne reste qu'une bille au centre";
+
+/** 
+ * Almost perfect message
+*/
 const std::string ALMOST_PERFECT_MESSAGE = "Bravo, vous avez gagne avec une seule bille restante. Au centre ce serait parfait";
+
+/** 
+ * Not bad message
+*/
 const std::string NOT_BAD_MESSAGE = "Pas mal, il ne reste que ";
+
+/** 
+ * Can do better message
+*/
 const std::string CAN_DO_BETTER_MESSAGE = "Vous pouvez faire mieux, il reste ";
+
+/** 
+* Peg translation
+*/
 const std::string PEG_TRANSLATION = "billes";
+
+/** 
+* Ask user input message
+*/
 const std::string ASK_USER_INPUT_MESSAGE = "Que voulez-vous faire? (q pour quittez, h pour l'aide)";
 
+/** 
+* Outbounds display value
+*/
 const std::string OUTBOUNDS_DISPLAY_VALUE = "  ";
+
+/** 
+* Hole display value
+*/
 const std::string HOLE_DISPLAY_VALUE = "..";
+
+/** 
+* Space
+*/
 const char SPACE = ' ';
+
+/** 
+* Column width
+*/
 const unsigned short COL_WIDTH = 3;
 
 /**
  * @brief display the game board
  *
- * @param[board] gameBoard to display
- * @return[void]
+ * @param gameBoard to display
  */
 void display(board gameBoard);
 
 /**
  * @brief display a message with the possible moves
  *
- * @param[vector] helpMoves the possible moves that can be made
- * @return[void]
+ * @param helpMoves the possible moves that can be made
  */
 void displayHelpMessage(std::vector<std::string> helpMoves);
 
 /**
  * @brief display an error message
  *
- * @return[void]
  */
 void displayErrorMessage();
 
 /**
  * @brief display the end game message
  *
- * @param[board] gameBoard in which the game was played
- * @param[unsigned] nbPegsLeft number of pegs left on the board
- * @return[void]
+ * @param gameBoard in which the game was played
+ * @param nbPegsLeft number of pegs left on the board
  */
 void displayEndGame(board gameBoard, unsigned nbPegsLeft);
 
 /**
- * @brief identify the sate of a cell and return the corresponding character
+ * @brief identify the state of a cell and return the corresponding character
  *
- * @param[board] game board
- * @param[int] row of the cell
- * @param[int] column of the cell
- * @return[void]
+ * @param game board
+ * @param row of the cell
+ * @param column of the cell
+ * @return According to the state of the cell, it returns the character that will fill the cell
  */
 std::string getDisplayValue(const board gameBoard, int row, int col);
 
 /**
  * @brief ask for user input
  *
- * @return[void]
- */
+  */
 void askForUserInput();
 
 /**
  * @brief display custom message if the user left the game
  *
- * @return[void]
  */
 void displayQuitMessage();
--- global.h
+++ global.h
@@ -5,31 +5,74 @@
 
 #ifndef LABO08_GLOBAL_H
 #define LABO08_GLOBAL_H
-
+/** 
+ * Board column size
+*/
 const unsigned short BOARD_COL_SIZE = 7;
+/** 
+ * Board row size
+*/
 const unsigned short BOARD_ROW_SIZE = 7;
+/** 
+ * Board margin
+*/
 const unsigned short MARGIN = 2;
+/** 
+ * Initial empty hole
+*/
 const unsigned short INITIAL_EMPTY_HOLE[] = {3, 3};
+/** 
+ * Coordinates size
+*/
 const unsigned short COORDINATES_SIZE = 2;
+/** 
+ * Row-only coordinates
+*/
 const unsigned short I_COORDINATE = 0;
+/** 
+ * Column-only coordinates
+*/
 const unsigned short J_COORDINATE = 1;
+/** 
+ * Input row position
+*/
 const unsigned short INPUT_ROW_POSITION = 0;
+/** 
+ * Input column size
+*/
 const unsigned short INPUT_COL_POSITION = 1;
+/** 
+ * Input direction position
+*/
+
 const unsigned short INPUT_DIRECTION_POSITION = 2;
+/** 
+ * Define a new type "board" that contain a multidimensionnal array with the variables numbers of row and column
+*/
 
 typedef char board[BOARD_COL_SIZE][BOARD_ROW_SIZE];
+
+/** 
+ * Define a new type "coordoinates" that contain an array with all the coordinates
+*/
 typedef unsigned coordinate[COORDINATES_SIZE];
 
+/** 
+ * Cell state (hole, peg, or outbounds)
+*/
 enum CellState {
-  HOLE = 'O',
-  PEG = '*',
-  OUTBOUNDS = 'V'
+    HOLE = 'O',
+    PEG = '*',
+    OUTBOUNDS = 'V'
 };
 
+/** 
+ * Direction (up, down, right, left)
+*/
 enum Direction {
-  UP = 'u',
-  DOWN = 'd',
-  RIGHT = 'r',
-  LEFT = 'l'
+    UP = 'u',
+    DOWN = 'd',
+    RIGHT = 'r',
+    LEFT = 'l'
 };
 #endif
